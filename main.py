6eceb2496c9c3cf1120847923c1b842a6aa38f3e
Shahed Al Mousselli <s.mosilli@icloud.com>
2026-07-07 22:51:03 +0100

    Add personal finance tracker functionality.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print("Personal Finance Tracker")
-print("------------------------")
+print("Welcome to the Personal Finance Tracker!")
+print("=" * 40)
 
 expense_name = input("Enter expense name: ")
 expense_amount = float(input("Enter expense amount: £"))
